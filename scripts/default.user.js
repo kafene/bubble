8e7b1583d2f5c25ef426a736ae45269f7399ecc9
kafene <matt@kafene.org>
2014-11-14 19:50:47 -0500

    spruce up default.user.js

--- scripts/default.user.js
+++ scripts/default.user.js
@@ -4,19 +4,25 @@ if (!window.frameElement) {
     alert('it works!');
 }
 
-// Open External Links in New Tab, add rel=noreferrer, remove a.ping
+// Open External Links in New Tab, add rel=noreferrer, remove a[ping]
 (function () {
     var host = window.location.host;
 
     var handleLink = function (a) {
+        // always remove a[ping]
         if (a.ping) {
             a.ping = '';
             a.removeAttribute('ping');
         }
 
+        // Only add noreferrer and target=_blank to links on different hosts
         if (a.href && 0 === a.href.indexOf('http') && a.host !== host) {
-            var rels = a.rel ? a.rel.trim().split(/\s+/) : [];
-            a.rel = rels.concat('noreferrer').join(' ');
+            if (a.rel) {
+                a.rel = a.rel.trim().split(/\s+/).concat('noreferrer').join(' ');
+            } else {
+                a.rel = 'noreferrer';
+            }
+
             a.target = '_blank';
         }
     };
@@ -27,8 +33,10 @@ if (!window.frameElement) {
             if (mutation.addedNodes) {
                 [].forEach.call(mutation.addedNodes, function (node) {
                     if (node instanceof HTMLAnchorElement) {
+                        // got a link
                         handleLink(node);
                     } else if (node.querySelectorAll) {
+                        // got an element containing links
                         [].forEach.call(node.querySelectorAll('a'), handleLink);
                     }
                 });
@@ -41,6 +49,6 @@ if (!window.frameElement) {
         characterData: false
     });
 
-    // run on existing nodes when DOM is loaded
+    // run on existing nodes immediately
     [].forEach.call(document.getElementsByTagName('a'), handleLink);
 })();
